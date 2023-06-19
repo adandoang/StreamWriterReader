8921c331284d5ddfc33436ddbcbf87226f171535
adandoang <akhdanjzif@gmail.com>
2023-06-19 15:30:04 +0700

    int main try catch

---
+++ manualExeption/manualExeption.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	try {
+		cout << "selamat belajar diprodi TI umy" << endl;
+		throw 0.5; //melemparkan sebuah integer maka
+		cout << "pernyataan tidak akan dieksekusi" << endl;
+	}
+	catch(int a) {
+		//blok ini akan dieksekusi
+		cout << "pengecualian akan dieksekusi" << endl;
+	}
+}
