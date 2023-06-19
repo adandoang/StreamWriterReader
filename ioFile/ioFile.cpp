42e32698ae5e6fab78c34cbbe651725c31fd184c
adandoang <akhdanjzif@gmail.com>
2023-06-19 14:58:16 +0700

    int main

--- ioFile/ioFile.cpp
+++ ioFile/ioFile.cpp
@@ -31,4 +31,14 @@ int main() {
 	//menunjuk ke sebuah file
 	infile.open("contohfile.txt");
 
+	cout << endl << ">= membuka dan membaca file" << endl;
+	//jika file ada maka
+	if (infile.is_open())
+	{
+		//melakukan perulangan setiap baris
+		while (getline(infile, baris))
+		{
+
+		}
+	}
 }
